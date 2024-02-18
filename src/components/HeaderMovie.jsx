5f12790b9c4b0420903e0955f78dd39be2bfd3a7
zanfinashabanii <zanfina.shabani31@gmail.com>
2024-02-18 08:04:31 +0100

    Fixed responsive

--- src/components/HeaderMovie.jsx
+++ src/components/HeaderMovie.jsx
@@ -12,12 +12,20 @@ export default function Header() {
 
   return (
     <Grid className="header" container spacing={2} sx={{ flexGrow: 1 }}>
-      <Grid xs={1}>
+      <Grid
+        style={{ alignSelf: "center" }}
+        item
+        xs={3}
+        sm={3}
+        md={2}
+        lg={2}
+        xl={2}
+      >
         <RouterLink to="/">
           <img src={logo} alt="Movie App" />
         </RouterLink>
       </Grid>
-      <Grid xs={8}>
+      <Grid style={{ alignSelf: "center" }} xs={6} sm={6} md={6} lg={6} xl={6}>
         <nav className="navigation">
           {arr.map((value, position) => (
             <Link
@@ -32,18 +40,30 @@ export default function Header() {
           ))}
         </nav>
       </Grid>
-      <Grid xs={3}>
-        <Input
-          color="warning"
-          size="sm"
-          variant="soft"
-          placeholder="Search a movie..."
-          value={search}
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
-        />
-        <button onClick={searchMovies}>Search Movie</button>
+      <Grid item container xs={12} sm={12} md={4} lg={4} xl={4}>
+        <Grid
+          style={{ alignSelf: "center" }}
+          item
+          xs={5}
+          sm={5}
+          md={5}
+          lg={6}
+          xl={6}
+        >
+          <Input
+            color="warning"
+            size="sm"
+            variant="soft"
+            placeholder="Search a movie..."
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+        </Grid>
+        <Grid item xs={6} sm={7} md={7} lg={6} xl={6}>
+          <button onClick={searchMovies}>Search Movie</button>
+        </Grid>
       </Grid>
     </Grid>
   );
--- src/components/HeaderTv.jsx
+++ src/components/HeaderTv.jsx
@@ -1,23 +1,24 @@
 import { useContext } from "react";
 import { TvShowContext } from "../pages/TvApp";
-import { Grid, Input, Link } from "@mui/joy";
+import { Grid, Input, Link, Button } from "@mui/joy";
 import logo from "../assets/logo.svg"; // Import your image here
 import { Link as RouterLink } from "react-router-dom";
 
 export default function Header() {
   let arr = ["Popular", "Kids", "Reality", "Crime"];
 
-  const { getTvShows, search, setSearch, searchTvShow } =
-    useContext(TvShowContext);
+  const { getTvShows, search, setSearch, searchTvShow } = useContext(
+    TvShowContext
+  );
 
   return (
-    <Grid className="header" container spacing={2} sx={{ flexGrow: 1 }}>
-      <Grid xs={1}>
+    <Grid  className="header" container spacing={2} sx={{ flexGrow: 1 }}>
+      <Grid style={{ alignSelf: 'center' }} item xs={3} sm={3} md={2} lg={2} xl={2}>
         <RouterLink to="/">
           <img src={logo} alt="TV Show App" />
         </RouterLink>
       </Grid>
-      <Grid xs={8}>
+      <Grid style={{ alignSelf: 'center' }} item xs={6} sm={6} md={6} lg={6}xl={6}>
         <nav className="navigation">
           {arr.map((value, position) => (
             <Link
@@ -32,18 +33,23 @@ export default function Header() {
           ))}
         </nav>
       </Grid>
-      <Grid xs={3}>
-        <Input
-          color="success"
-          size="sm"
-          variant="soft"
-          placeholder="Search a TV show..."
-          value={search}
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
-        />
-        <button onClick={searchTvShow}>Search Tv Show</button>
+      <Grid item container xs={12} sm={12} md={4} lg={4} xl={4} >
+        <Grid style={{ alignSelf: 'center' }}item xs={5} sm={5} md={5} lg={6} xl={6}>
+          <Input
+            color="success"
+            size="sm"
+            variant="soft"
+            placeholder="Search a TV show..."
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+            
+          />
+        </Grid>
+        <Grid item xs={6} sm={7} md={7}  lg={6} xl={6}>
+        <button onClick={searchTvShow}>Search Tv show</button>
+        </Grid>
       </Grid>
     </Grid>
   );
